317603b359cbb7d82ec978b012c538a2a3ff0152
Onugha Charles <onughacharles11@gmail.com>
2026-09-09 05:44:51 +0100

    tweeked the y axis and parameters a bit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Example: Predicting daily activities (represented as numbers)
 # 0=Sleep, 1=Work, 2=Exercise, 3=Social, 4=Leisure
 
-historical_activities = [0, 1, 2, 1, 3, 1, 4, 0, 1, 2, 1, 3, 1, 4, 0]
+historical_activities = [0, 1, 1, 1, 1, 1, 2, 0, 1, 1, 1, 1, 1, 4]
 
 def calculate_probabilities(sequence):
     """
@@ -38,7 +38,7 @@ plt.bar(activities, probabilities, color='steelblue')
 plt.title('Digital Twin: Activity Prediction Probabilities')
 plt.ylabel('Probability')
 plt.xlabel('Activity')
-plt.ylim(0, 0.5)
+plt.ylim(0, 1.0)
 for i, v in enumerate(probabilities):
     plt.text(i, v + 0.01, f'{v:.1%}', ha='center')
 plt.show()
